cd3edb799baadf68a163df035b52dc9089020273
Dima Os <osadchenko.d@gmail.com>
2024-06-17 09:45:02 +0300

    add microfixes

--- js/init-schedule.js
+++ js/init-schedule.js
@@ -3,7 +3,7 @@ import DaySchedule from "./day-schedule.js";
 const initSchedule = () => {
   const workingHoursStart = '09:00';
   const workingHoursEnd = '21:00';
-  const busyTimes = [
+  const busy = [
     {start: '10:30', stop: '10:50'},
     {start: '18:40', stop: '18:50'},
     {start: '14:40', stop: '15:50'},
@@ -11,7 +11,7 @@ const initSchedule = () => {
     {start: '20:05', stop: '20:20'},
   ];
 
-  const schedule = new DaySchedule(workingHoursStart, workingHoursEnd, busyTimes);
+  const schedule = new DaySchedule(workingHoursStart, workingHoursEnd, busy);
 
   console.log(schedule.getPossibleAppointments());
 }
